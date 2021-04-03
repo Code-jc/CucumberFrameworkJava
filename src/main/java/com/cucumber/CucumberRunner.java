d469d6aabcd2802c09b46bc87c1c6dd1bf830d11
Carlos Guerra <carlos.vlz@outlook.com>
2021-04-03 01:36:28 -0600

    Created basic structure and Cucumber Runner class

---
+++ src/main/java/com/cucumber/CucumberRunner.java
@@ -0,0 +1,20 @@
+
+package com.cucumber;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+		//Path to the scripts to Run
+		features = {""},
+		tags = {""}
+		)
+public class CucumberRunner {
+
+
+
+	
+	
+}
